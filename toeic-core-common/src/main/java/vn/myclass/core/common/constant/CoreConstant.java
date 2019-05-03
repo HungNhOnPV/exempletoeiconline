d000942e9a8f11efba75b9a84019f79905c125ef
HungNhOnPV <anhungx9propro@gmail.com>
2019-05-03 19:19:47 +0700

    init project

---
+++ toeic-core-common/src/main/java/vn/myclass/core/common/constant/CoreConstant.java
@@ -0,0 +1,7 @@
+package vn.myclass.core.common.constant;
+
+public class CoreConstant {
+    // khong thay doi gia tri duoc
+    public static final String SORT_ASC = "2";
+    public  static final String SORT_DESC = "1";
+}

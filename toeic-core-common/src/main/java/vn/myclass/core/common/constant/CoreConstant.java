0d629234850412f76d19adf083462a5a6cb65708
HungNhOnPV <anhungx9propro@gmail.com>
2019-05-03 19:01:58 +0700

    delete all

--- toeic-core-common/src/main/java/vn/myclass/core/common/constant/CoreConstant.java
+++
@@ -1,7 +0,0 @@
-package vn.myclass.core.common.constant;
-
-public class CoreConstant {
-    // khong thay doi gia tri duoc
-    public static final String SORT_ASC = "2";
-    public  static final String SORT_DESC = "1";
-}

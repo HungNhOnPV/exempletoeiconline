812f7f095cf88e2153f692cd3f9ff894c6d56a5a
HungNhOnPV <anhungx9propro@gmail.com>
2019-05-07 02:52:49 +0700

    create function login v3

--- toeic-api-test/src/test/java/vn/myclass/api/test/LoginTest.java
+++ toeic-api-test/src/test/java/vn/myclass/api/test/LoginTest.java
@@ -1,4 +1,31 @@
 package vn.myclass.api.test;
 
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+import vn.myclass.core.dao.UserDao;
+import vn.myclass.core.daoimpl.UserDaoImpl;
+import vn.myclass.core.persistence.entity.UserEntity;
+
 public class LoginTest {
+    private final Logger log = Logger.getLogger(this.getClass());
+    @Test
+    public void checkIsUserExist() {
+        UserDao userDao = new UserDaoImpl();
+        String name = "phamviethung";
+        String password = "hung2711";
+        UserEntity entity = userDao.isUserExist(name, password);
+        if (entity != null) {
+            log.error("login success");
+        } else {
+            log.error("login fail");
+        }
+    }
+
+    public void checkFindRoleByUser() {
+        UserDao userDao = new UserDaoImpl();
+        String name = "phamviethung";
+        String password = "hung2711";
+        UserEntity entity = userDao.findRoleByUser(name, password);
+        log.error(entity.getRoleEntity().getRoleID() +"-"+ entity.getRoleEntity().getName());
+    }
 }

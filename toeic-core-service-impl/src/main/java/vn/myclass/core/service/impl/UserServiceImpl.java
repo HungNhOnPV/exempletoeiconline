205af2fb595ab74e415d5954b7983fa380e996df
HungNhOnPV <anhungx9propro@gmail.com>
2019-05-08 22:25:08 +0700

    done create function login

--- toeic-core-service-impl/src/main/java/vn/myclass/core/service/impl/UserServiceImpl.java
+++ toeic-core-service-impl/src/main/java/vn/myclass/core/service/impl/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package vn.myclass.core.service.impl;
 
-public class UserServiceImpl {
+import vn.myclass.core.dao.UserDao;
+import vn.myclass.core.daoimpl.UserDaoImpl;
+import vn.myclass.core.dto.UserDTO;
+import vn.myclass.core.persistence.entity.UserEntity;
+import vn.myclass.core.service.UserService;
+import vn.myclass.core.utils.UserBeanUtil;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDTO isUserExist(UserDTO userDTO) {
+        UserDao userDao = new UserDaoImpl();
+        UserEntity entity = userDao.isUserExist(userDTO.getName(), userDTO.getPassword());
+        return UserBeanUtil.entity2Dto(entity);
+    }
+
+    @Override
+    public UserDTO findRoleByUser(UserDTO userDTO) {
+        UserDao userDao = new UserDaoImpl();
+        UserEntity entity = userDao.findRoleByUser(userDTO.getName(), userDTO.getPassword());
+        return UserBeanUtil.entity2Dto(entity);
+    }
 }
--- toeic-web-logic/src/main/java/vn/myclass/web/controller/admin/LoginController.java
+++ toeic-web-logic/src/main/java/vn/myclass/web/controller/admin/LoginController.java
@@ -3,6 +3,9 @@ package vn.myclass.web.controller.admin;
 import org.apache.log4j.Logger;
 import vn.myclass.command.UserCommand;
 import vn.myclass.core.dto.UserDTO;
+import vn.myclass.core.service.UserService;
+import vn.myclass.core.service.impl.UserServiceImpl;
+import vn.myclass.core.web.common.WebConstant;
 import vn.myclass.core.web.utils.FormUtil;
 
 import javax.servlet.RequestDispatcher;
@@ -16,16 +19,36 @@ import java.io.IOException;
 @WebServlet("/login.html")
 public class LoginController extends HttpServlet {
     private transient final Logger log = Logger.getLogger(this.getClass());
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("/views/web/login.jsp");
         rd.forward(request, response);
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-//        UserCommand command = FormUtil.populate(UserCommand.class, request);
-//        UserDTO pojo = command.getPojo();
-        RequestDispatcher rd = request.getRequestDispatcher("/views/web/login.jsp");
-        rd.forward(request, response);
+        UserCommand command = FormUtil.populate(UserCommand.class, request);
+        UserDTO pojo = command.getPojo();
+        UserService userService = new UserServiceImpl();
+        try {
+            if (userService.isUserExist(pojo) != null) {
+                if (userService.findRoleByUser(pojo) != null && userService.findRoleByUser(pojo).getRoleDTO() != null) {
+                    if (userService.findRoleByUser(pojo).getRoleDTO().getName().equals(WebConstant.ROLE_ADMIN)) {
+                        request.setAttribute(WebConstant.ALERT, WebConstant.TYPE_SUCCESS);
+                        request.setAttribute(WebConstant.MESSAGE_RESPONSE, "Bạn là admin");
+                    } else if (userService.findRoleByUser(pojo).getRoleDTO().getName().equals(WebConstant.ROLE_USER)) {
+                        request.setAttribute(WebConstant.ALERT, WebConstant.TYPE_SUCCESS);
+                        request.setAttribute(WebConstant.MESSAGE_RESPONSE, "Bạn là user");
+                    }
+                }
+            }
+        } catch (NullPointerException e) {
+            log.error(e.getMessage(), e);
+            request.setAttribute(WebConstant.ALERT, WebConstant.TYPE_ERROR);
+            request.setAttribute(WebConstant.MESSAGE_RESPONSE, "Tên hoặc mật khẩu sai");
+        }
+            RequestDispatcher rd = request.getRequestDispatcher("/views/web/login.jsp");
+            rd.forward(request, response);
+        }
     }
-}
